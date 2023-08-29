4409ec8fdde297f5dd915e3d4795d7073b0267d6
Giovanni Juliao <gjuliao32@gmail.com>
2023-08-29 15:50:16 -0500

    Added validations to link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,11 +2,22 @@ class Link < ApplicationRecord
     validates_uniqueness_of :short_url
     validates_presence_of :original_url, :short_url
     validate :original_url_format
+    validate :short_url_format
 
     def original_url_format
-        uri = Uri.parse(original_url || '')
+        uri = URI.parse(original_url || '')
         if uri.host.nil?
             errors.add(:original_url, 'Invalid url')
         end
+    end
+
+    def short_url_format
+        unless short_url.match?(/\A[A-Z]{5}\z/)
+            errors.add(:short_url, "must be 5 characters long and consist of capital letters")
+        end
+    end
+
+    def url_short
+        "https://localhost:3000/#{short_url}"
     end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'check if url link is valid' do
+    link = Link.new(
+      original_url: 'https://www.google.com',
+      short_url: 'ABCDE'
+    )
+    link.save
+    expect(link.valid?).to be(true)
+  end
+
+  it 'invalid if url is fake' do
+    link = Link.new(
+      original_url: 'asdfasdfasdfasdfasdf',
+      short_url: 'ABCDE'
+    )
+    expect(link.valid?).to be(false)
+  end
+
+  it 'invalid if short_url is not well formated is fake' do
+    link = Link.new(
+      original_url: 'https://www.google.com',
+      short_url: 'abcdef'
+    )
+    expect(link.valid?).to be(false)
+  end
+
+  
 end
