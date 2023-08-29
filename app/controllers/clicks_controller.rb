d7cf6411eabf88668f5ee52b2783f2afd637efb3
Giovanni Juliao <gjuliao32@gmail.com>
2023-08-29 17:26:29 -0500

    Added the click post in index and the create

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -1,5 +1,26 @@
 class ClicksController < ApplicationController
 
+    def show
+        link = Link.find_by(id: @click.link_id)
+        redirect_to link.original_url, allow_other_host: true
+    end
+
     def create
+        @click = Click.new(click_params)
+        link = Link.find_by(id: @click.link_id)
+
+        if @click.save
+            flash[:success] = 'Click saved succesfully'
+            redirect_to link.original_url, allow_other_host: true
+        else
+            flash[:error] = 'Click not saved'
+            redirect_to links_path
+        end
+    end
+
+    private
+
+    def click_params
+        params.permit(:link_id)
     end
 end
