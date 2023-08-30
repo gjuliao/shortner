b21678613e42563621f3830f4a7119dc6eabe42c
Giovanni Juliao <gjuliao32@gmail.com>
2023-08-30 10:45:29 -0500

    Added clicks controller test

--- spec/requests/clicks_spec.rb
+++ spec/requests/clicks_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Clicks", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "create click" do
+    link = Link.create(
+      original_url: 'http://www.google.com',
+      short_url: 'ABCDE'
+    )
+
+    click = link.clicks.new
+    
+
+    click.save
+
+    expect(click.valid?).to be(true)
   end
 end
